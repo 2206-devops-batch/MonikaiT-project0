9658be74d7be2c54f2864029919d16ff7e9a0a80
Your Name <you@example.com>
2022-06-14 15:16:38 -0400

    Added text

--- project0.py
+++ project0.py
@@ -1,13 +1,30 @@
-
+#!/usr/bin/env bash
 #Budget app for expenses. To later be connected to the To-do list.
 #Possible code Login to access to-do and budget.
 #Possible use code: Budget app
-#import math1
-import csv
+#import math
+#add dictionary,add test, logging module, budget log
 
+import csv #implements classes to read and write data from csv format.
+#csv.reader iterates over lines in a given csvfile.
+#csv.writer converts user data into delimited strings on the given file-like object.
+#with open("'to--do.csv', 'w', newline = ''") as csvfile:
+#fieldname = ['âœ“', 'Date', 'Task']
+#???df = csv.writer
+import unittest
+import subprocess
+import math
+import pdb
+import turtle
+import logging
+import pickle
+logging.basicConfig(filename='todo.log', level = logging.DEBUG)
+logging.basicConfig(filename='budget.log', level = logging.INFO)
 print("Welcome!")
-print("Ready to do?")
+
+budget = {"monthly": "The next 4 weeks", "Yearly" : "The next 365 days ",}
 while True:
+    print("Ready to do?")
     enterexit = input("Answer with Y or N: ")
     if enterexit.upper() == "N":
         print("Exiting...")
@@ -17,7 +34,7 @@ while True:
         print("Let's get started.")
         print("What would you like to do today?")
         todo_budget = input("Enter 'N' for BUDGET or 'Y' for TODO: ")
-        if todo_budget.upper() == "N": #This is chunky, I'd like to make it less chunky.
+        if todo_budget.upper() == "N":  # This is chunky, I'd like to make it less chunky.
             print("Let's get budgeting!")
             checking = (int(input("Checking amount: ")))
             housing = (int(input("Housing: ")))
@@ -27,19 +44,26 @@ while True:
             food = (int(input("Food: ")))
             misc = (int(input("Misc: ")))
             total_expenses = (housing + saving + bills + transportation + food + misc)
-            print(str(total_expenses ) + " is your total expense for this month")
+            logging.info(str(total_expenses) + " is your total expense for this month")
+            print(str(total_expenses) + " is your total expense for this month")
             months_leftover = (int(checking - total_expenses))
-            print(str(months_leftover ) + " is your remainder this month.")
-            exit()
+            logging.info(str(months_leftover) + " is your remainder this month.")
+            print(str(months_leftover) + " is your remainder this month.")
+            continue
             # I want to print results to a file here
-        elif todo_budget.upper() == "Y": # I want to import a csv file here
-            df = open ('todo.csv', 'rt')
+        elif todo_budget.upper() == "Y":  # I want to import a csv file here
+            df = open('todo.csv', 'rt')
             rows = csv.reader(df)
             for row in rows:
                 print(row)
-              #  exitinterview = input("Ready to go? 'Y' or 'N':  ")
-              #  if exitinterview.upper() == "Y":
-              #   print("goodbye!")
-               #     exit()
-               # elif exitinterview.upper() == "N":
-               # enterexit = input("Answer with Y or N: ")
+                logging.debug(row)
+            break
+print("Exit Interview")
+exitinterview = input("Ready to go? 'Y' or 'N':  ")
+        # elif exitinterview.upper() == "N":
+        # enterexit = input("Answer with Y or N: ")
+while exitinterview.upper() != "Y":
+    print("goodbye!")
+    exit()
+    if exitinterview.upper() == "N":
+        continue
