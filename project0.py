ea77a5fd528d439aa23a56387d748c45a8b1e7a8
mtinsonk <you@example.com>
2022-06-15 18:22:45 -0400

    Added Text

--- project0.py
+++ project0.py
@@ -2,8 +2,8 @@
 #Budget app for expenses. To later be connected to the To-do list.
 #Possible code Login to access to-do and budget.
 #Possible use code: Budget app
-#import math
-#add dictionary,add test, logging module, budget log kk
+import math
+#add dictionary,add test, logging module, budget log
 
 import csv #implements classes to read and write data from csv format.
 #csv.reader iterates over lines in a given csvfile.
@@ -22,21 +22,28 @@ logging.basicConfig(filename='todo.log', level = logging.DEBUG)
 logging.basicConfig(filename='budget.log', level = logging.INFO)
 print("Welcome!")
 
-budget = {"monthly": "The next 4 weeks", "Yearly" : "The next 365 days ",}
+budget_dict = {"monthly": "The next 4 weeks", "Yearly" : "The next 365 days ",}
+with open('budget_pick.pkl','wb') as pickle_file: # serialize an object without having to add extra code.
+    pickle.dump(budget_dict, pickle_file) #converts obj to byte stream that can be loaded on a buffer or disk.
+    #I'd like to print all user inpur to a text or csv file.
+
 while True:
     print("Ready to do?")
     enterexit = input("Answer with Y or N: ")
-    if enterexit.upper() == "N":
+    if enterexit.upper() == "N": #built in function to change any inpt to uppercase.
         print("Exiting...")
-        exit()
+        exit()# exit the program completely
+
     else:
         enterexit.upper() == "Y"
         print("Let's get started.")
         print("What would you like to do today?")
         todo_budget = input("Enter 'N' for BUDGET or 'Y' for TODO: ")
+
+
         if todo_budget.upper() == "N":  # This is chunky, I'd like to make it less chunky.
             print("Let's get budgeting!")
-            checking = (int(input("Checking amount: ")))
+            checking = (int(input("Checking amount: ")))#int allows for the input interger to be printed
             housing = (int(input("Housing: ")))
             saving = (int(input("Saving: ")))
             bills = (int(input("Bills: ")))
@@ -45,12 +52,14 @@ while True:
             misc = (int(input("Misc: ")))
             total_expenses = (housing + saving + bills + transportation + food + misc)
             logging.info(str(total_expenses) + " is your total expense for this month")
-            print(str(total_expenses) + " is your total expense for this month")
+            print(str(total_expenses) + " is your total expense for this month") #str turns the interger into a string.
             months_leftover = (int(checking - total_expenses))
             logging.info(str(months_leftover) + " is your remainder this month.")
             print(str(months_leftover) + " is your remainder this month.")
-            continue
+            continue #continues the loop at the start.
             # I want to print results to a file here
+
+
         elif todo_budget.upper() == "Y":  # I want to import a csv file here
             df = open('todo.csv', 'rt')
             rows = csv.reader(df)
@@ -58,6 +67,7 @@ while True:
                 print(row)
                 logging.debug(row)
             break
+
 print("Exit Interview")
 exitinterview = input("Ready to go? 'Y' or 'N':  ")
         # elif exitinterview.upper() == "N":
