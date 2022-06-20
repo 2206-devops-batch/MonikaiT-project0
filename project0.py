bd645013f48a95bf6d386ea75407846c40e75b45
mtinsonk <mtinsonk@gmail.com>
2022-06-20 18:44:26 -0400

    Updated

--- project0.py
+++ project0.py
@@ -34,73 +34,64 @@ while True:
         print("Exiting...")
         exit()# exit the program completely
 
-    else:
-        enterexit.upper() == "Y"
+    elif enterexit.upper() == "Y":
         print("Let's get started.")
         print("What would you like to do today?")
-        todo_budget = input("Enter 'N' for BUDGET or 'Y' for TODO: ")
-
+        todo_budget = input("Enter 'N' for BUDGET, 'Y' for TODO, or 'x' to exit: ")
 
         if todo_budget.upper() == "N":  # This is chunky, I'd like to make it less chunky.
             print("Let's get budgeting!")
-            checking = (int(input("Checking amount: ")))#int allows for the input interger to be printed
-            housing = (int(input("Housing: ")))
-            savings = (int(input("Savings: ")))
-            bills = (int(input("Bills: ")))
-            transportation = (int(input("Transportation: ")))
-            food = (int(input("Food: ")))
-            misc = (int(input("Misc: ")))
-            total_expenses = (housing + savings + bills + transportation + food + misc)
-            logging.info(str(total_expenses) + " is your total expense for this month")
-            print(str(total_expenses) + " is your total expense for this month") #str turns the interger into a string.
-            months_leftover = (int(checking - total_expenses))
-            logging.info(str(months_leftover) + " is your remainder this month.")
-            print(str(months_leftover) + " is your remainder this month.")
-            with open('budget.csv', 'w', newline='') as k:
-                budgetwrite = csv.writer(k)
-                dm = datetime.date.today()
-                budgetwrite.writerow(('Date', 'Expense for this month', 'Remainder for this month', 'Checking', 'Housing', 'Saving', 'Bills', 'Transportation', 'Food', 'Misc'))
-                budgetwrite.writerow((dm, total_expenses, months_leftover, checking, housing , savings, bills, transportation, food, misc))
-            continue #continues the loop at the start.
+            while True:
+                try:
+                    checking = (int(input("Checking amount: ")))#int allows for the input interger to be printed
+                    housing = (int(input("Housing: ")))
+                    savings = (int(input("Savings: ")))
+                    bills = (int(input("Bills: ")))
+                    transportation = (int(input("Transportation: ")))
+                    food = (int(input("Food: ")))
+                    misc = (int(input("Misc: ")))
+                    total_expenses = (housing + savings + bills + transportation + food + misc)
+                    logging.info(str(total_expenses) + " is your total expense for this month")
+                    print(str(total_expenses) + " is your total expense for this month") #str turns the interger into a string.
+                    months_leftover = (int(checking - total_expenses))
+                    logging.info(str(months_leftover) + " is your remainder this month.")
+                    print(str(months_leftover) + " is your remainder this month.")
+                    with open('budget.csv', 'w', newline='') as k:
+                        budgetwrite = csv.writer(k)
+                        dm = datetime.date.today()
+                        budgetwrite.writerow(('Date', 'Expense for this month', 'Remainder for this month', 'Checking', 'Housing', 'Saving', 'Bills', 'Transportation', 'Food', 'Misc'))
+                        budgetwrite.writerow((dm, total_expenses, months_leftover, checking, housing , savings, bills, transportation, food, misc))
+                        k.close()
+                        break
+                         #continues the loop at the start.
+                except ValueError:
+                    print("you did not enter a valid number.")
+
             # I want to print results to a file here
 
 
         elif todo_budget.upper() == "Y":  # I want to import a csv file here
+            print("Welcome to your Todo list!")
             df = open('todo.csv', 'r')
             rows = csv.reader(df)
             with open('todo.csv', 'r') as csvfile:
                 for row in rows:
                     print(row[1:4])
                     logging.debug(row)
-            break
-
-print("Exit Interview")
-exitinterview = input("Ready to go? 'Y' or 'N':  ")
-        # elif exitinterview.upper() == "N":
-        # enterexit = input("Answer with Y or N: ")
-while exitinterview.upper() == "Y":
-    print("goodbye!")
-    exit()
-    if exitinterview.upper() == "N":
-        print("")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                df.close()
+        elif todo_budget.upper() == "X":
+            print("Exit Interview")
+            exitinterview = input("Ready to go? 'Y' or 'N':  ")
+            # elif exitinterview.upper() == "N":
+            # enterexit = input("Answer with Y or N: ")
+            if exitinterview.upper() == "Y":
+                print("goodbye!")
+                exit()
+            elif exitinterview.upper() == "N":
+                continue
+            else:
+                print("Response not valid. Enter \'y\' or \'n\'.")
+        else:
+            print("Response not valid. Enter \'y\' , \'n\', or \'x\'.")
+    else:
+        print("Response not valid. Enter \'y\' or \'n\'.")
--- test_project0.py
+++ test_project0.py
@@ -3,3 +3,4 @@ class Testtotal_expenses(unittest.TestCase):
     def test_upper(self):
         self.assertEqual('y'.upper(), 'Y')
         self.assertEqual('n'.upper(), 'N')
+        self.assertEqual('x'.upper(), 'X')
